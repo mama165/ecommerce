b4a1d63468039bd71e8d706d8750323650c61763
mama165 <maelnana1@gmail.com>
2019-10-15 08:37:24 +0200

    wip: cleaning combinations, etc

--- src/main/java/fr/ecommerce/app/implementations/PriceResolverImpl.java
+++ src/main/java/fr/ecommerce/app/implementations/PriceResolverImpl.java
@@ -1,44 +1,12 @@
 package fr.ecommerce.app.implementations;
 
 import fr.ecommerce.app.PriceResolver;
-import fr.ecommerce.app.models.OperationPercent;
 import fr.ecommerce.app.models.Price;
-import org.paukov.combinatorics3.Generator;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class PriceResolverImpl implements PriceResolver {
-    private static final BigDecimal TWENTY_FIVE_PERCENT = new BigDecimal("3.75");
-    private static final BigDecimal TWENTY_PERCENT = new BigDecimal("3.2");
-    private static final BigDecimal TEN_PERCENT = new BigDecimal("2.7");
-    private static final BigDecimal FIVE_PERCENT = new BigDecimal("1.9");
-
-    private static final List<List<Integer>> FIVE_PERCENT_LIST =
-            Generator.permutation(0, 0, 0, 1, 1)
-                    .simple()
-                    .stream()
-                    .collect(Collectors.toList());
-
-    private static final List<List<Integer>> TEN_PERCENT_LIST =
-            Generator.permutation(0, 0, 1, 1, 1)
-                    .simple()
-                    .stream()
-                    .collect(Collectors.toList());
-
-    private static final List<List<Integer>> TWENTY_PERCENT_LIST =
-            Generator.permutation(0, 1, 1, 1, 1)
-            .simple()
-            .stream()
-            .collect(Collectors.toList());
-
-    private static final List<List<Integer>> TWENTY_FIVE_PERCENT_LIST =
-            Generator.permutation(1, 1, 1, 1, 1)
-                    .simple()
-                    .stream()
-                    .collect(Collectors.toList());
-
     /**
      * We can start by finding some groupes of five articles
      * if c minimum in [a,b,c,d,e}
